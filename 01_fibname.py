1358e1f779f994519165df6aa13b50c2db4860dd
tommirrington <36197693+tommirrington@users.noreply.github.com>
2018-02-22 15:05:16 +0000

    Create 01_fibname.py
    Original submission for exercises 1 and 2

---
+++ 01_fibname.py
@@ -0,0 +1,33 @@
+# Tom Mirrington
+
+#Exercise 1 response
+#My name is Tom, so the first and last letter of my name (T + M = 20 + 13) give the number  33. The 33rd Fibonacci number is 3524578. 
+
+#Exercise 2
+# A program that displays Fibonacci numbers using people's names.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+name = "Mirrington"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
+print("Fibonacci number", x, "is", ans)
+
