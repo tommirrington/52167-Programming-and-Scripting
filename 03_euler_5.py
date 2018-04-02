fa7a255f68b2a8754923b9860b3da0fbbf4470f4
Tom Mirrington <tommirrington@MacBook-Pro-8.local>
2018-04-02 15:51:15 +0100

    edited printed text

--- 03_euler_5.py
+++ 03_euler_5.py
@@ -10,7 +10,7 @@ while y > 0:
  for x in range(1, 21): 
      y = y + (n % x)
  if y == 0:
-    print('The smallest positive number that is divisible by 20 is {}'.format(n))
+    print('The smallest positive number that is divisible by all of the numbers from 1 to 20 is {}'.format(n))
  else:
     n = n + 20 #must be a factor of 20 so increment by 20 to speed up process
     print(n)
