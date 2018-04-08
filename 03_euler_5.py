05f69e80385fe173b9a02e9b2e25176181843325
Tom Mirrington <tommirrington@MacBook-Pro-8.local>
2018-04-08 15:06:21 +0100

    refromatted annotation

--- 03_euler_5.py
+++ 03_euler_5.py
@@ -2,13 +2,16 @@
 #solution to https://projecteuler.net/problem=5
 
 #determine initial values 
+#use y as counter, set to 1 to satisfy condition that y > 0
+
 n = 20 
-y = 1 # used as a counter, set to 1 to satisfy condition that y > 0
+y = 1 
 
 
  #reset y to 0 then iterate though range 1 to 20, if the sum of the modulus value for entire range is zero
  #then n is divisible by all values from 1 to 20, if not then increment n by 20 and test again
- 
+ #the soolution must be a factor of 20 so increment by 20 to speed up process
+
 while y > 0:
  y = 0 
  for x in range(1, 21): 
@@ -16,7 +19,7 @@ while y > 0:
  if y == 0:
     print('The smallest positive number that is divisible by all of the numbers from 1 to 20 is {}'.format(n))
  else:
-    n = n + 20 #must be a factor of 20 so increment by 20 to speed up process
+    n = n + 20 
     print(n)
 
 #The smallest positive number that is divisible by all of the numbers from 1 to 20 is 232792560
