2610fad1cb5909ce9f2cd51d9998f497003305bb
Tom Mirrington <tommirrington@MacBook-Pro-8.local>
2018-04-08 15:04:50 +0100

    reformatted annotation

--- 02_collatz.py
+++ 02_collatz.py
@@ -1,17 +1,22 @@
 # Tom Mirrington 2018-02-12
 # Ecercise 2 submission
 
-#propmpt user for an integer
+#prompt user for an integer
 n = int(input("Please enter a positive integer: "))
 
-while n >= 1: #satisfies the condition that n is a positive integer
+
+#satisfy the condition that n is a positive integer
+#determine if the value is an even number, if even then divide by 2
+#if the value is not an even number or 1 then it must be odd therefore multiply by three and add one
+
+while n >= 1: 
     if n == 1: 
         break
-    elif n % 2 == 0: #determines if the value is an even number, if even then divide by 2
+    elif n % 2 == 0: 
         n = int(n/2)
         print(n)
         
-    else: #if the value is not an even number or 1 then it must be odd therefore multiply by three and add one
+    else: 
         n = (n * 3) + 1
         print(n)
      
